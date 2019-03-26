11b6c2de9198ef4af6f654811a2cb130f615a4ff
Phil <philbroderick12@gmail.com>
2019-03-26 19:53:45 +0000

    Successfully retrieved events from Cosmos DB

---
+++ EventsMaster.Auth/Controllers/EventController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventsMaster.Api.Models;
+using EventsMaster.Api.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Options;
+
+namespace EventsMaster.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly CosmosDBSettings _dbSettings;
+        private DocumentDBRepository<Event> _dbContext;
+
+        public EventController(IOptions<CosmosDBSettings> settings)
+        {
+            _dbSettings = settings.Value;
+            _dbContext = new DocumentDBRepository<Event>("EventsMaster", "Event");
+        }
+
+        [HttpGet, Route("")]
+        public async Task<IActionResult> GetAllEventsAsync()
+        {
+            var events = await _dbContext.GetItemsAsync();
+            return Ok(events);
+        }
+    }
+}
---
+++ EventsMaster.Auth/DocumentDBRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsMaster.Api
+{
+    public class DocumentDBRepository<T> where T : class
+    {
+        private static string _databaseId;
+        private static string _collectionId;
+        private static DocumentClient _client;
+
+        public DocumentDBRepository(string databaseId, string collectionId)
+        {
+            _databaseId = databaseId;
+            _collectionId = collectionId;
+            _client = new DocumentClient(new Uri("https://eventsmaster.documents.azure.com:443/"), "n9bgGZ7AIFIisJWVbHpLmdbdFByrIY9bmimnTGhns7dJqworsRgjVdQ2cwT7ZSuZ3xKnVKfnjZGDbtiMLHpVuw==");
+        }
+
+        public async Task<IEnumerable<T>> GetItemsAsync()
+        {
+            IDocumentQuery<T> query = _client.CreateDocumentQuery<T>(
+                UriFactory.CreateDocumentCollectionUri(_databaseId, _collectionId),
+                new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true })
+                .AsDocumentQuery();
+
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+
+            return results;
+        }
+    }
+}
