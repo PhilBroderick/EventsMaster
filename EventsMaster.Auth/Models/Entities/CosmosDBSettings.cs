6c7daedcd976e1561905cdf7a1147af425de8335
Phil <philbroderick12@gmail.com>
2019-03-26 19:30:08 +0000

    Get Cosmos DB Details from appsettings.json

---
+++ EventsMaster.Auth/Models/Entities/CosmosDBSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsMaster.Api.Models.Entities
+{
+    public class CosmosDBSettings
+    {
+        public string EndpointUri { get; set; }
+        public string PrimaryKey { get; set; }
+    }
+}
--- EventsMaster.Auth/Startup.cs
+++ EventsMaster.Auth/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EventsMaster.Api.Models.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -18,12 +19,14 @@ namespace EventsMaster.Auth
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             Configuration = configuration;
+            HostingEnvironment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -51,6 +54,8 @@ namespace EventsMaster.Auth
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
                     };
                 });
+
+            services.Configure<CosmosDBSettings>(Configuration.GetSection("CosmosDB"));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
