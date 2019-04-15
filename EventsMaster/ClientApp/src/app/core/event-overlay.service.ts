bdf98dedaf51742796326d8d97fc8396538f8ef2
Phil <philbroderick12@gmail.com>
2019-04-15 21:59:18 +0100

    CDK Overlay configuration - need to add dark background class

--- EventsMaster/ClientApp/src/app/core/event-overlay.service.ts
+++ EventsMaster/ClientApp/src/app/core/event-overlay.service.ts
@@ -1,21 +1,57 @@
 import { Injectable } from '@angular/core';
-import { Overlay } from '@angular/cdk/overlay';
+import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 
 import { EventOverlayComponent } from '../events/event-overlay.component';
+
+interface EventOverlayConfig {
+  panelClass?: string;
+  hasBackdrop?: boolean;
+  backdropClass?: string;
+}
+
+const DEFAULT_CONFIG: EventOverlayConfig = {
+  hasBackdrop: true,
+  backdropClass: 'dark-backdrop',
+  panelClass: 'tm-event-preview-panel'
+}
 
 @Injectable()
 export class EventOverlayService {
 
   constructor(private overlay: Overlay) { }
 
+  open(config: EventOverlayConfig = {}) {
 
-  open() {
+    const dialogConfig = { ...DEFAULT_CONFIG, ...config };
 
-    const overlayRef = this.overlay.create();
+    const overlayRef = this.createOverlay(dialogConfig);
 
     const eventPreviewPortal = new ComponentPortal(EventOverlayComponent);
 
     overlayRef.attach(eventPreviewPortal);
+  }
+
+  private getOverlayConfig(config: EventOverlayConfig): OverlayConfig {
+    const positionStrategy = this.overlay.position()
+      .global()
+      .centerHorizontally()
+      .centerVertically();
+
+    const overlayConfig = new OverlayConfig({
+      hasBackdrop: config.hasBackdrop,
+      backdropClass: config.backdropClass,
+      panelClass: config.panelClass,
+      scrollStrategy: this.overlay.scrollStrategies.block(),
+      positionStrategy
+    });
+
+    return overlayConfig;
+  }
+
+  private createOverlay(config: EventOverlayConfig) {
+    const overlayConfig = this.getOverlayConfig(config);
+
+    return this.overlay.create(overlayConfig);
   }
 }
