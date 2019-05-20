688eba334c43834c5fed7a7ef5daa2f653ce6e3e
Phil <philbroderick12@gmail.com>
2019-05-20 19:55:40 +0100

    Allow users to update an events image from overlay

--- EventsMaster/ClientApp/src/app/events/event-overlay.component.ts
+++ EventsMaster/ClientApp/src/app/events/event-overlay.component.ts
@@ -5,8 +5,9 @@ import { EVENT_DIALOG_DATA } from "./event-overlay.tokens";
 import { EventOverlayRef } from './event-overlay-ref';
 import { LoginComponent } from '../login/login.component';
 import { LoginService } from '../core/login.service';
-import { Event } from '@angular/router';
 import { EventService } from '../core/event.service';
+import { UploadService } from '../core/image-upload.service';
+import { Event } from '../shared/models/event.model';
 
 const ANIMATION_TIMINGS = '400ms cubic-bezier(0.25, 0.8, 0.25, 1)';
 const ESCAPE = 27;
@@ -19,6 +20,8 @@ const ESCAPE = 27;
 
 export class EventOverlayComponent {
 
+  selectedFile = null;
+
   @HostListener('document:keydown', ['$event']) private handleKeydown(event: KeyboardEvent) {
     if (event.keyCode === ESCAPE) {
       this.dialogRef.close();
@@ -29,12 +32,28 @@ export class EventOverlayComponent {
     public dialogRef: EventOverlayRef,
     public loginService: LoginService,
     private eventService: EventService,
+    private uploadService: UploadService,
     @Inject(EVENT_DIALOG_DATA) public event: any) { }
   
-  updateEvent(event) {
-    this.eventService.updateEvent(event).subscribe(event => {
-      console.log("event updated " + event.name)
-      this.dialogRef.close();
-    });
+  updateEvent(event: Event) {
+    if (this.selectedFile !== null) {
+      this.uploadService.uploadFile(this.selectedFile).subscribe(res => {
+        let imageUri = res.uri;
+        event.imageUrl = imageUri;
+        console.log(event.imageUrl);
+        this.eventService.updateEvent(event).subscribe(event => {
+          this.dialogRef.close();
+        });
+      })
+    } else {
+      this.eventService.updateEvent(event).subscribe(event => {
+        this.dialogRef.close();
+      })
+    }
+  }
+
+  onFileSelected(event) {
+    this.selectedFile = event.target.files[0];
+    console.log(this.selectedFile);
   }
 }
